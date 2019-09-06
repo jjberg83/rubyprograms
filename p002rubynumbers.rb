67172b1c2e588bc72a9fddd43c7c2bae8978f129
Jorund <jorund@Jrunds-iMac.local>
2019-09-06 20:16:35 +0200

    Adding some more number lines

--- p002rubynumbers.rb
+++ p002rubynumbers.rb
@@ -8,6 +8,7 @@ Usual operators:
 / division
 =end
 
+=begin
 puts 1 + 2
 puts 2 + 3
 # Integer division
@@ -16,3 +17,10 @@ puts 3 / 2
 puts 10 - 11
 puts 1.5 / 2.6
 puts 3.2 / 2
+puts 7 % 3
+puts 7.0.modulo 3
+=end
+
+puts nil || 2019
+@myName ||= 'Ola Nordmann'
+puts @myName
